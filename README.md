bd97fe173c10ee0adabbad905d0229b5dee1024a
qudongfang <qudongfang@gmail.com>
2014-05-06 13:36:38 +0800

    aqueducts logstash agent

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-Aqueducts-client-packager
-=========================
+aqueducts项目日志数据采集agent，基于logstash，数据发布到kafka。
 
-Project Aqueducts client packager for rhel4
+修改点：
 
-
-# Dependicies
-
-1. ruby1.9.3
-1. bundler
-1. fpm
+1. logstash/input/file 增加host in_time字段，删除path字段，设置默认stat_interval = 0.75,即750ms
+2. logstash/input/file 依赖ruby-filewatch gem，修改tail.rb中sysread buffer，减少发送延迟，提高发送能力
+3. logstash/input/file 读取文件数据依赖libffi.so，jar包中默认libffi.so依赖glibc 2.5, RHEL 4.4无法启动，重新编译libffi.so，替换
+4. logstash/output/kafka jruby-kafka异步发送，从zk获取kafka broker最新机器列表
+5. logstash/output/kafka 自动附加host idc ip product service 字段
--- package.sh
+++ package.sh
@@ -1,105 +1,26 @@
-#!/bin/bash
-set -x
+#!/usr/bin/env bash
 
-project=Aqueducts
-version=1.0.0
-output_path=/home/work/
+tmp_dir=`date +%Y%M%d%H%m`
+mkdir ${tmp_dir}
+cd ${tmp_dir}
 
-project_path=/opt/${project}
-rm -rf ${project_path}
-install_path=${project_path}/embedded
-cache_path=${project_path}/cache
-src_path=${project_path}/src
-pkg_path=${project_path}/pkg
+mkdir output src
 
-LDFLAGS="-L${install_path}/lib -I${install_path}/include"
-CFLAGS="-I${install_path}/include -L${install_path}/lib"
-export LDFLAGS CFLAGS
+cd src
 
-rm -rf ${project_path}
-mkdir -p ${install_path} ${cache_path} ${src_path} ${pkg_path}
+wget -q http://download.aqueducts.baidu.com/jdk_1.6.tar.gz && tar xzf jdk_1.6.tar.gz &&  rm -f jdk_1.6.tar.gz
+JAVA_HOME=`pwd`/jdk1.6.0_27_x64
+PATH=${JAVA_HOME}/bin:$PATH
 
-## 1: bin
-softwares=(
-yaml-0.1.4
-ruby-1.9.3-p484
-)
+git clone https://github.com/castomer/logstash.git && git checkout aqueducts
+cd logstash && make flatjar && cp -f ./build/logstash-*.jar ../output && cd ../
 
-remote_addrs=(
-http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
-http://ruby.taobao.org/mirrors/ruby/1.9/ruby-1.9.3-p484.tar.gz
-)
+git clone https://github.com/castomer/logstash-kafka.git && git checkout aqueducts
+cd logstash-kafka && make flatjar && cp -f ./build/logstash-*.jar ../output && cd ../
 
-software_count=${#softwares[@]}
-index=0
+cd ../output
 
-while [ "${index}" -lt "${software_count}" ] ; do
-  software=${softwares[${index}]}
-  remote_addr=${remote_addrs[${index}]}
+mkdir -p ~/nfs/download/${tmp_dir}/
+cp -f logstash-kafka-*.jar ~/nfs/download/${tmp_dir}/ 
 
-  wget ${remote_addr} -P ${cache_path}
-  cd ${cache_path}
-  tar -xzf ${software}.tar.gz
-  cd ${software}
-  ./configure --prefix=${install_path}
-  make -j 3
-  make -j 3 install
-  
-  ((index++))
-done
 
-# 2: rubygems
-software=rubygems-1.8.24
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz -P ${cache_path}
-cd ${cache_path}
-tar -xzvf ${software}.tgz
-cd ${cache_path}/${software}
-${install_path}/bin/ruby setup.rb
-
-## 3: gems
-gems=(
-fluentd
-zookeeper
-consistent-hashing
-fluent-plugin-redis-publish
-rest-client
-poseidon
-mime-types
-)
-
-index=0
-while [ "${index}" -lt "${#gems[@]}" ] ; do
-  gem=${gems[${index}]}
-  ${install_path}/bin/gem install ${gem} --no-rdoc --no-ri  || exit 11
-  ((index++))
-done
-
-## 4: gems from git
-source_gems=(
-fluent-plugin-kafka
-)
-
-index=0
-while [ "${index}" -lt "${#source_gems[@]}" ] ; do
-  gem=${source_gems[${index}]}
-  cd ${src_path}
-  git clone git://github.com/ops-baidu/${gem}.git   &&\
-  cd ${gem}                                         &&\
-  ${install_path}/bin/gem build ${gem}.gemspec      &&\
-  ${install_path}/bin/gem install -l ${src_path}/${gem}/${gem}-*.gem --no-rdoc --no-ri || exit 11
-  ((index++))
-done
-
-## bug : install fpm twice , one from local , one from gem , the reason is fix this problem is expensive.
-cd ${src_path}
-gem=fpm
-git clone git://github.com/ops-baidu/${gem}.git
-cd ${gem}
-${install_path}/bin/gem build ${gem}.gemspec
-${install_path}/bin/gem install -l ${src_path}/${gem}/${gem}-*.gem --no-rdoc --no-ri
-${install_path}/bin/gem install ${gem} --no-rdoc --no-ri || exit 11
-
-## package
-rm -rf ${cache_path} ${src_path} ${install_path}/share
-cd
-${install_path}/bin/fpm -s dir -t rpm -n "${project}" -v ${version} -p ${output_path} -f ${project_path}  || exit 12 
