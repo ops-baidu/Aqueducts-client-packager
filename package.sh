f26e79019d28cf3c5ed16242c9847ebab4cd251d
qudongfang <qudongfang@gmail.com>
2014-05-08 19:42:51 +0800

    typo bugfix

--- package.sh
+++ package.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
+set -x
 
 tmp_dir=`date +%Y%m%d%H%M`
-output_dir=~/nsf/download/logstash/${tmp_dir}
-current_dir=~/nsf/download/logstash/current
+output_dir=~/nfs/download/logstash/${tmp_dir}
+current_dir=~/nfs/download/logstash/current
 JAVA_HOME=`pwd`/jdk1.6.0_27_x64
 PATH=${JAVA_HOME}/bin:$PATH
 
-if [ ! -d "${JAVA_HOME}" ]; then  
+if [ ! -d "${JAVA_HOME}" ]; then
   wget -q http://download.aqueducts.baidu.com/jdk_1.6.tar.gz && tar xzf jdk_1.6.tar.gz &&  rm -f jdk_1.6.tar.gz
-fi  
+fi
 
 mkdir -p ${tmp_dir} ${output_dir}
 cd ${tmp_dir}
 
 git clone -b aqueducts http://gitlab.baidu.com/qudongfang/logstash.git
 cd logstash && make flatjar
-cp -f ./build/logstash-* ${output_dir} && cd ../
+cp ./build/logstash-* ${output_dir} && cd ../
+rm -f ${current_dir} && ln -s ${output_dir} ${current_dir}
 
 git clone -b aqueducts http://gitlab.baidu.com/qudongfang/logstash-kafka.git
 cd logstash-kafka && make flatjar
-mkdir -p ~/nfs/download/logstash/${tmp_dir}/ && cp -f ./build/logstash-* ${outout_dir} && rm -f ${current_dir} && ln -s ${output_dir} ${current_dir}
-
+mkdir -p ~/nfs/download/logstash/${tmp_dir}/ && cp -f ./build/logstash-* ${outout_dir}
